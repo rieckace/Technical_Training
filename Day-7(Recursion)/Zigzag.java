ecfa526c93b2350478cf2f86de470c1ae8ebda96
Rikesh Yadav <155945432+rieckace@users.noreply.github.com>
2025-03-16 23:24:14 +0530

    Update Zigzag.java

--- Day-7(Recursion)/Zigzag.java
+++ Day-7(Recursion)/Zigzag.java
@@ -1,22 +1,3 @@
-
-public class Zigzag {
-    static void zigzag(int n) {
-        if (n == 0) {
-            return;
-        }
-        System.out.print(n);
-        zigzag(n - 1);
-        System.out.print(n);
-        zigzag(n - 1);
-        System.out.print(n);
-    }
-
-    public static void main(String[] args) {
-        int n = 3;
-        zigzag(n);
-    }
-}
-=======
 public class Zigzag {
     static void zigzag(int n) {
         if (n == 0) {
