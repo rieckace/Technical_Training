042b24c6d6d322be8e82952929015e4454b1b5cb
Rikesh Yadav <155945432+rieckace@users.noreply.github.com>
2025-03-16 23:21:08 +0530

    Update Main.java

--- Day-7(Recursion)/Main.java
+++ Day-7(Recursion)/Main.java
@@ -1,28 +1,3 @@
-
-import java.util.Scanner;
-
-public class Main {
-
-    static int factorial(int n) {
-      
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        return n * factorial(n - 1);
-    }
-
-    public static void main(String[] args) {
-      
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        int result = factorial(n);
-        System.out.println("Factorial of " + n + " is: " + result);
-    }
-}
-=======
 import java.util.Scanner;
 
 public class Main {
