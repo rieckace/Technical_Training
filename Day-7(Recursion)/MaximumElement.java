480bde0c232f6cb1617bdd302cef6ad1a6af2853
Rikesh Yadav <155945432+rieckace@users.noreply.github.com>
2025-03-16 23:22:33 +0530

    Update MaximumElement.java

--- Day-7(Recursion)/MaximumElement.java
+++ Day-7(Recursion)/MaximumElement.java
@@ -1,22 +1,3 @@
-
-public class MaximumElement {
-    public static void main(String [] args){
-        int [] arr = {1,2,3,8,10,15};
-        System.out.print("The maximum element in the array is: ");
-        System.out.println(maxElement(arr, 0, arr.length-1));
-    }
-
-    
-    public static int maxElement(int []arr, int start, int end){
-        if(start == end){
-            return arr[start];
-        }
-        int max = maxElement(arr, start+1, end);
-        return Math.max(arr[start], max);
-    }
-    
-}
-=======
 public class MaximumElement {
     public static void main(String [] args){
         int [] arr = {1,2,3,8,10,15};
